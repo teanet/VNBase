bf49ba292ecf4960253005833f7e0bada9ac7bf6
e.tyutyuev <e.tyutyuev@2gis.ru>
2020-04-18 22:06:01 +0700

    Better push handler

---
+++ VNBase/Classes/Categories/Data.swift
@@ -0,0 +1,6 @@
+public extension Data {
+	func pushToken() -> String {
+		let token = self.map({ String(format: "%02.2hhx", $0)}).joined()
+		return token
+	}
+}
--- VNBase/Classes/Services/BasePushHandler.swift
+++ VNBase/Classes/Services/BasePushHandler.swift
@@ -1,8 +1,14 @@
-public extension Data {
-	func pushToken() -> String {
-		let token = self.map({ String(format: "%02.2hhx", $0)}).joined()
-		return token
-	}
+public protocol IBackgroundPushHandler {
+	func handle(message: PushMessage, completion: @escaping (UIBackgroundFetchResult) -> Void)
+}
+
+public protocol INotificationResponseHandler {
+	func handle(response: UNNotificationResponse, completion: @escaping () -> Void)
+}
+
+public protocol IPushHandlersProvider: AnyObject {
+	func backgroundPushHandlers() -> [IBackgroundPushHandler]
+	func notificationResponseHandlers() -> [INotificationResponseHandler]
 }
 
 open class BasePushHandler: NSObject {
@@ -11,8 +17,10 @@ open class BasePushHandler: NSObject {
 	public private(set) var isRespondedToNotificationsRequestAlert: Bool = false
 	public private(set) var lastResponse: UNNotificationResponse?
 	public private(set) var tokenData: Data?
+	public weak var provider: IPushHandlersProvider?
 
-	public override init() {
+	public init(provider: IPushHandlersProvider?) {
+		self.provider = provider
 		super.init()
 		self.userNC.delegate = self
 		self.userNC.getNotificationSettings { (settings) in
@@ -23,19 +31,8 @@ open class BasePushHandler: NSObject {
 		}
 	}
 
-	open func shouldShowMessage(for notification: UNNotification) -> Bool {
-		return true
-	}
-
-	open func didShowMessage(_ message: PushMessage) {
-	}
-
-	open func didSelectMessage(
-		_ message: PushMessage,
-		response: UNNotificationResponse,
-		completionHandler: @escaping () -> Void
-	) {
-		completionHandler()
+	open func shouldShowMessage(for notification: UNNotification) -> UNNotificationPresentationOptions {
+		return [.alert]
 	}
 
 	public func registerForRemote() {
@@ -46,20 +43,44 @@ open class BasePushHandler: NSObject {
 		self.tokenData = token
 	}
 
+	public func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+
+		let handlers = self.provider?.backgroundPushHandlers() ?? []
+		guard !handlers.isEmpty else {
+			completionHandler(.noData)
+			return
+		}
+
+		let message = PushMessage(push: userInfo)
+		let group = DispatchGroup()
+		var finalResult: UIBackgroundFetchResult = .noData
+		for handler in handlers {
+			group.enter()
+			handler.handle(message: message) { (result) in
+				if result == .failed {
+					finalResult = .failed
+				} else if result == .newData && finalResult != .failed {
+					finalResult = .newData
+				}
+				group.leave()
+			}
+		}
+		group.notify(queue: .main) {
+			completionHandler(finalResult)
+		}
+	}
+
 }
 
 extension BasePushHandler: UNUserNotificationCenterDelegate {
 
-	public func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-
-		if self.shouldShowMessage(for: notification) {
-			completionHandler(.alert)
-		} else {
-			completionHandler([])
-		}
-
-		let message = PushMessage(push: notification.request.content.userInfo)
-		self.didShowMessage(message)
+	public func userNotificationCenter(
+		_ center: UNUserNotificationCenter,
+		willPresent notification: UNNotification,
+		withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+	) {
+		let options = self.shouldShowMessage(for: notification)
+		completionHandler(options)
 	}
 
 	// Тап по пушу в открытом или закрытом приложении
@@ -69,8 +90,34 @@ extension BasePushHandler: UNUserNotificationCenterDelegate {
 		withCompletionHandler completionHandler: @escaping () -> Void
 	) {
 		self.lastResponse = response
-		let message = PushMessage(push: response.notification.request.content.userInfo)
-		self.didSelectMessage(message, response: response, completionHandler: completionHandler)
+		let handlers = self.provider?.notificationResponseHandlers() ?? []
+		guard !handlers.isEmpty else {
+			completionHandler()
+			return
+		}
+
+		let group = DispatchGroup()
+		for handler in handlers {
+			group.enter()
+			handler.handle(response: response) {
+				group.leave()
+			}
+		}
+		group.notify(queue: .main) {
+			completionHandler()
+		}
 	}
 
+}
+
+public extension UNNotificationResponse {
+	func message() -> PushMessage {
+		return self.notification.message()
+	}
+}
+
+public extension UNNotification {
+	func message() -> PushMessage {
+		return PushMessage(push: self.request.content.userInfo)
+	}
 }
