0e1d19dac9d3431d93b0791b42feaf185ce02e07
e.tyutyuev <e.tyutyuev@2gis.ru>
2020-05-01 14:05:44 +0700

    Update image named

--- VNBase/Classes/Categories/UIImageView.swift
+++ VNBase/Classes/Categories/UIImageView.swift
@@ -1,13 +1,18 @@
-import UIKit
-
 public extension UIImageView {
 
-	static func named(_ name: String?) -> UIImageView {
+	static func named(
+		_ name: String?,
+		renderingMode: UIImage.RenderingMode = .automatic
+	) -> UIImageView {
 		let image: UIImage? = {
 			guard let name = name, !name.isEmpty else { return nil }
 			return UIImage(named: name)
 		}()
 		return UIImageView(image: image)
+	}
+
+	static func templatedNamed(_ name: String?) -> UIImageView {
+		return self.named(name, renderingMode: .alwaysTemplate)
 	}
 
 }
