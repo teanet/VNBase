28df13b9e77d0bff3cbd7367d9ded4832f79e67f
e.tyutyuev <e.tyutyuev@2gis.ru>
2020-05-07 13:23:00 +0700

    Userdefaults property wrapper

---
+++ VNBase/Classes/Utils/UD.swift
@@ -0,0 +1,26 @@
+@propertyWrapper
+public struct UD<T: Codable> {
+
+	private let key: String
+	private let defaultValue: T
+
+	public init(wrappedValue initialValue: T, key: String) {
+		self.key = key
+		self.defaultValue = initialValue
+	}
+
+	public var wrappedValue: T {
+		get {
+			guard let data = UserDefaults.standard.object(forKey: key) as? Data else {
+				return self.defaultValue
+			}
+			let value = try? JSONDecoder().decode(T.self, from: data)
+			return value ?? self.defaultValue
+		}
+		set {
+			let data = try? JSONEncoder().encode(newValue)
+			UserDefaults.standard.set(data, forKey: self.key)
+		}
+	}
+
+}
