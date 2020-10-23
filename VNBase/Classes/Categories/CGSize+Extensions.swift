33b53f4b7f2d1538ef8e717e68d25bb167357091
e.tyutyuev <e.tyutyuev@2gis.ru>
2020-10-23 13:02:50 +0700

    Update size height to static

--- VNBase/Classes/Categories/CGSize+Extensions.swift
+++ VNBase/Classes/Categories/CGSize+Extensions.swift
@@ -1,6 +1,6 @@
 public extension CGSize {
 
-	func height(_ height: CGFloat) -> CGSize {
+	static func height(_ height: CGFloat) -> CGSize {
 		CGSize(width: UIView.noIntrinsicMetric, height: height)
 	}
 
