c98339079971bf471506337cddda833da75f7388
e.tyutyuev <e.tyutyuev@2gis.ru>
2021-03-14 13:47:37 +0700

    UIMode

---
+++ VNBase/Classes/Utils/UIMode+TraitCollection.swift
@@ -0,0 +1,45 @@
+public enum UIMode {
+	case day
+	case night
+}
+
+extension UITraitCollection {
+
+	static var currentMode: UIMode {
+		#if compiler(>=5.1)
+		if #available(iOS 13.0, *) {
+			return self.current.mode
+		} else {
+			return .night
+		}
+		#else
+		return .night
+		#endif
+	}
+
+	var mode: UIMode {
+		if #available(iOS 12.0, *), self.userInterfaceStyle == .dark {
+			return .night
+		}
+		return .day
+	}
+
+}
+
+public extension UIColor {
+	static func color(_ light: UIColor, dark: UIColor) -> UIColor {
+		if #available(iOS 13.0, *) {
+			return UIColor { $0.mode == .day ? light : dark }
+		} else {
+			return dark
+		}
+	}
+
+	func cgColor(with traitCollection: UITraitCollection) -> CGColor {
+		if #available(iOS 13.0, *) {
+			return self.resolvedColor(with: traitCollection).cgColor
+		} else {
+			return self.cgColor
+		}
+	}
+}
