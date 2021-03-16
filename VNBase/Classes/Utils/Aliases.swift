917abbebbf7cb2535f7eda4f1e687e4c9910b72a
e.tyutyuev <e.tyutyuev@2gis.ru>
2021-03-16 17:09:46 +0700

    Void result

--- VNBase/Classes/Utils/Aliases.swift
+++ VNBase/Classes/Utils/Aliases.swift
@@ -1,3 +1,4 @@
 public typealias VoidBlock = (() -> Swift.Void)
 public typealias BoolBlock = ((Bool) -> Swift.Void)
 public typealias ErrorBlock = ((Error) -> Swift.Void)
+public typealias VoidResultBlock = ((Swift.Result<Void, Error>) -> Swift.Void)
