edac26362017fc0e71ea52743ffbdd8375792a07
e.tyutyuev <e.tyutyuev@2gis.ru>
2021-03-17 22:55:26 +0700

    UIView extensions

--- VNBase/Classes/Categories/UIView.swift
+++ VNBase/Classes/Categories/UIView.swift
@@ -2,6 +2,11 @@ import UIKit
 import SnapKit
 
 public extension UIView {
+	class func make(color: UIColor = .clear) -> UIView {
+		let view = UIView()
+		view.backgroundColor = color
+		return view
+	}
 
 	func addSubview(_ subview: UIView, withConstraints closure: (_ make: ConstraintMaker) -> Void) {
 		subview.translatesAutoresizingMaskIntoConstraints = false
@@ -23,6 +28,29 @@ public extension UIView {
 
 	func findSubview<T: UIView>(_ type: T.Type) -> T? {
 		return self.findSubview { $0 is T } as? T
+	}
+
+	func setCornerRadius(_ radius: CGFloat) {
+		self.layer.setCornerRadius(radius)
+	}
+
+}
+
+public protocol Buildable {}
+public extension Buildable where Self: UIView {
+	@discardableResult
+	func with(_ closure: (Self) -> Void) -> Self {
+		closure(self)
+		return self
+	}
+}
+extension UIView: Buildable {}
+
+public extension CALayer {
+
+	func setCornerRadius(_ radius: CGFloat) {
+		self.cornerRadius = radius
+		self.masksToBounds = true
 	}
 
 }
