2a0217834757e189e1253469e8f7be95f7589f81
e.tyutyuev <e.tyutyuev@2gis.ru>
2021-04-09 16:40:33 +0700

    Fix alpha

--- VNBase/Classes/Categories/Bool+Extensions.swift
+++ VNBase/Classes/Categories/Bool+Extensions.swift
@@ -1,5 +1,5 @@
 public extension Bool {
 	var alpha: CGFloat { self ? 1 : 0 }
-	var highlightedAlpha: CGFloat { self ? self.highlightedAlpha : 1 }
+	var highlightedAlpha: CGFloat { self ? CGFloat.highlightedAlpha : 1 }
 	var isHiddenAlpha: CGFloat { self ? 0 : 1 }
 }
