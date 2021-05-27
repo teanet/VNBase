496fe901c8bf7e3c33c136b660dbaba77352bbc6
e.tyutyuev <e.tyutyuev@2gis.ru>
2021-05-28 00:07:33 +0700

    Fix deps

--- Package.swift
+++ Package.swift
@@ -9,14 +9,14 @@ let package = Package(
 		.watchOS(.v5),
 	],
 	products: [
-		.library(name: "VNBase", targets: ["VNBase", "VNEssential"]),
+		.library(name: "VNBase", targets: ["VNBase"]),
 		.library(name: "VNEssential", targets: ["VNEssential"]),
 	],
 	dependencies: [
 		.package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1"))
 	],
 	targets: [
-		.target(name: "VNBase", path: "VNBase/Classes"),
+		.target(name: "VNBase", dependencies: ["VNEssential"], path: "VNBase/Classes"),
 		.target(name: "VNEssential",  path: "VNBase/Essential"),
 	],
 	swiftLanguageVersions: [
--- VNBase/Classes/View/Table/Cell/TableSectionVM.swift
+++ VNBase/Classes/View/Table/Cell/TableSectionVM.swift
@@ -1,5 +1,5 @@
 import UIKit
-import 
+import VNEssential
 
 class TableSectionId: Id<String> {}
 
