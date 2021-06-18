961e49e79429e8936b71d8f845eb1d040c30cca1
e.tyutyuev <e.tyutyuev@2gis.ru>
2021-06-18 17:29:38 +0700

    IInvalidating: AnyObject

--- Example/VNBase/ViewController.swift
+++ Example/VNBase/ViewController.swift
@@ -5,14 +5,16 @@ class ViewController: UIViewController {
 
 	override var supportedInterfaceOrientations: UIInterfaceOrientationMask { .portrait }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
 		self.navigationItem.rightBarButtonItems = [
 			UIBarButtonItem(title: "Table", style: .plain, target: self, action: #selector(self.tableTap)),
 			UIBarButtonItem(title: "Rotate", style: .plain, target: self, action: #selector(self.rotateTap)),
 		]
-    }
+		let vc = Child()
+		self.dgs_add(vc: vc, view: self.view)
+	}
 
 	@objc private func tableTap() {
 		let vc = TableDemoVC(viewModel: TableDemoVM())
@@ -22,5 +24,39 @@ class ViewController: UIViewController {
 	@objc private func rotateTap() {
 		let vc = LandscapeVC(viewModel: LandscapeVM())
 		self.navigationController?.pushViewController(vc, animated: true)
+	}
+
+	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+		super.viewWillTransition(to: size, with: coordinator)
+		print("viewWillTransition>>>>>\(type(of: self)) \(self.view.bounds)")
+	}
+
+	override func viewWillLayoutSubviews() {
+		super.viewWillLayoutSubviews()
+		print("viewWillLayoutSubviews>>>>>\(type(of: self)) \(self.view.bounds)")
+	}
+
+
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		print("viewDidLayoutSubviews>>>>>\(type(of: self)) \(self.view.bounds)")
+	}
+}
+
+class Child: UIViewController {
+	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+		super.viewWillTransition(to: size, with: coordinator)
+		print("viewWillTransition>>>>>\(type(of: self)) \(self.view.bounds)")
+	}
+
+	override func viewWillLayoutSubviews() {
+		super.viewWillLayoutSubviews()
+		print("viewWillLayoutSubviews>>>>>\(type(of: self)) \(self.view.bounds)")
+	}
+
+
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		print("viewDidLayoutSubviews>>>>>\(type(of: self)) \(self.view.bounds)")
 	}
 }
--- VNBase/Essential/IInvalidating.swift
+++ VNBase/Essential/IInvalidating.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-public protocol IInvalidating {
+public protocol IInvalidating: AnyObject {
 	func invalidate()
 }
