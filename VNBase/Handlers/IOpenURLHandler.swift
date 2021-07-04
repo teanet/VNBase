35d5eaef1c8178efab4e3142c16c8234579fe22a
e.tyutyuev <e.tyutyuev@2gis.ru>
2021-07-04 21:36:25 +0700

    Handle scene IOpenURLHandler

--- VNBase/Handlers/IOpenURLHandler.swift
+++ VNBase/Handlers/IOpenURLHandler.swift
@@ -4,4 +4,26 @@ public protocol IOpenURLHandler {
 
 	func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool
 
+	@available(iOS 13.0, *)
+	func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>)
+}
+
+public extension IOpenURLHandler {
+	@available(iOS 13.0, *)
+	func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+		for context in URLContexts {
+			var options = [UIApplication.OpenURLOptionsKey : Any]()
+			if let annotation = context.options.annotation {
+				options[.annotation] = annotation
+			}
+			if #available(iOS 14.5, *), let eventAttribution = context.options.eventAttribution {
+				options[.eventAttribution] = eventAttribution
+			}
+			options[.openInPlace] = context.options.openInPlace
+			if let sourceApplication = context.options.sourceApplication {
+				options[.sourceApplication] = sourceApplication
+			}
+			_ = self.application(UIApplication.shared, open: context.url, options: options)
+		}
+	}
 }
