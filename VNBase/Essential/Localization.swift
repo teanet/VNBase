0b3f4a03933d87f785423a950dedde49463d8585
e.tyutyuev <e.tyutyuev@2gis.ru>
2021-07-08 15:38:10 +0700

    L10N with bundle

--- VNBase/Essential/Localization.swift
+++ VNBase/Essential/Localization.swift
@@ -1,24 +1,25 @@
 import Foundation
 
-public class Testing {
-	public static var isUnitTest = false
+public class Config {
+	public static var disableLocalization = false
+	public static var bundle: Bundle = .main
 }
 
 // swiftlint:disable:next identifier_name
 public func L10N(_ string: String) -> String {
-	if Testing.isUnitTest {
+	if Config.disableLocalization {
 		return string
 	} else {
-		return NSLocalizedString(string, comment: "")
+		return NSLocalizedString(string, bundle: Config.bundle, comment: "")
 	}
 }
 
 // swiftlint:disable:next identifier_name
 public func L18N(_ format: String, value: Int) -> String {
-	if Testing.isUnitTest {
+	if Config.disableLocalization {
 		return format
 	} else {
-		let format = NSLocalizedString(format, comment: "")
+		let format = NSLocalizedString(format, bundle: Config.bundle, comment: "")
 		return String(format: format, locale: Locale.current, value)
 	}
 }
