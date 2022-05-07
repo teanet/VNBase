501e809bfe41ca5c5d6022f990918b075ca56d1e
e.tyutyuev <e.tyutyuev@2gis.ru>
2022-05-07 13:52:24 +0700

    Add animation shugar

---
+++ VNBase/Classes/Categories/UIView+Animations.swift
@@ -0,0 +1,35 @@
+@objc public extension UIView {
+	/// Синтаксический сахар
+	///
+	/// Метод используется чтобы каждый раз не писать портянку проверок
+	/// 
+	///     if animated {
+	///         UIView.animation(block)
+	///     } else {
+	///         block()
+	///     }
+	///
+	/// - Parameters:
+	///   - animated: выполнять animations в анимационном блоке или нет
+	class func performAnimated(
+		_ animated: Bool,
+		withDuration duration: TimeInterval,
+		delay: TimeInterval = 0,
+		options: UIView.AnimationOptions = [],
+		animations: @escaping () -> Void,
+		completion: ((Bool) -> Void)? = nil
+	) {
+		if animated {
+			self.animate(
+				withDuration: duration,
+				delay: delay,
+				options: options,
+				animations: animations,
+				completion: completion
+			)
+		} else {
+			animations()
+			completion?(true)
+		}
+	}
+}
